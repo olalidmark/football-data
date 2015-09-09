0cb77f959bbd1682b28184ae69d4b983e0845595
fantomen <ola.lidmark-eriksson@adeprimo.se>
2015-09-09 15:56:05 +0200

    added swe d1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='football-data',
-    version='0.1.6',
+    version='0.1.7',
     packages=['fbdata'],
     url='https://github.com/olalidmark/football-data',
     license='MIT',
